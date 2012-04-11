176a14b4190349298502d6d23fd325de9a1841ea
Andy Davies <andy.davies@gmail.com>
2012-04-11 10:36:25 +0100

    adding grunt build process, adjusting Swipe threshold

---
+++ grunt.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    min: {
+    	dist: {
+    		src: ['js/TouchLayerController.js', 'js/TapController.js', 'js/DragController.js', 'js/SwipeController.js', 'js/touchLayer.js'],
+    		dest: 'jsTouchLayer.min.js',
+    	    separator: ';'
+    	}
+    }
+  });
+
+  grunt.registerTask('default', 'min');
+
+};
